cddee675ea84678407936fe0f5cfe01ee3456e7e
geekboyswag <geekboyswag@users.noreply.github.com>
2015-04-28 06:56:03 +0800

    Create example.rb

---
+++ example.rb
@@ -0,0 +1,7 @@
+require_relative 'lib';
+
+chat = '';
+chat_tagServer = 'sX.chatango.com'; # replace X with ur chat tagServer (function to find servers coming soon) [Ex: forevertest tagserver is 79]
+nigger = '';
+password = '';
+bot = Racism.new("#{chat}", "#{chat_tagServer}", "#{nigger}", "#{password}");
